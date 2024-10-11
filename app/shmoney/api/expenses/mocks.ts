7d4b4d75dee33533a7db289bc68a37081d948063
Yakir Rabinovich <yagiro+gh@gmail.com>
2024-10-11 16:07:57 +0300

    commit to test cicd trigger

--- app/shmoney/api/expenses/mocks.ts
+++ app/shmoney/api/expenses/mocks.ts
@@ -23,4 +23,7 @@ export const expenses: Expense[] = [
     createExpense('2', 'aya', 'Plants', 320, CurrencyId.ILS,
         new Date(Date.now() - 3600000 * 30),
     ),
+    createExpense('2', 'aya', 'Honey', 28, CurrencyId.ILS,
+        new Date(Date.now() - 3600000 * 31),
+    ),
 ]
