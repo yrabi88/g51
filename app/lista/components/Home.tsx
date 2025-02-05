e1aad46b4f043f082b39b341eeb629605c3d618a
Yakir Rabinovich <yagiro+gh@gmail.com>
2025-02-06 01:17:30 +0200

    add svg and change style

--- app/lista/components/Home.tsx
+++ app/lista/components/Home.tsx
@@ -8,11 +8,28 @@ import { List } from '../types'
 import Row from '@/app/components/layout/Row'
 import Link from 'next/link'
 import Button from '@/app/components/basic/button/Button'
+import XMark from '@/app/icons/x-mark.svg'
 
 // todo: code style
 
 interface Props {
     initialLists: List[]
+}
+interface ListInfoProps {
+    list: List
+    onRemove: () => void
+}
+
+function ListInfo(props: ListInfoProps) {
+    const { list, onRemove } = props
+    return (
+        <Link href={`/lista/${list.id}`}>
+            <Row key={list.id} className='gap-5 p-3 bg-amber-200 shadow-sm justify-between'>
+                {list.name}
+                <XMark className='cursor-pointer' onClick={ (e: Event) => (onRemove(), e.preventDefault())}/>
+            </Row>
+        </Link>
+    )
 }
 
 export default function Home(props: Props) {
@@ -35,12 +52,13 @@ export default function Home(props: Props) {
         try {
             await addList(newListName)
             setListsStale(true)
+            setNewListName('')
         } catch(err) {
             console.error('failed to add list', newListName, err)
         }
     }
 
-    const removeListAndRefetch = async (listId: string) => {
+const removeListAndRefetch = async (listId: string) => {
         try {
             await removeList(listId)
             setListsStale(true)
@@ -50,19 +68,17 @@ export default function Home(props: Props) {
     }
     
     return (
-        <Col className="p-7 gap-10 items-center bg-gray-100 text-gray-600 h-screen main-font">
-            <div className={`text-6xl`}>Lista</div>
-            <Col className="gap-3">
+        <Col className="p-7 gap-10 bg-gray-100 text-gray-600 h-screen">
+            <Col className="gap-3 w-5/6 items-stretch">
                 <div className={`text-2xl`}>My Lists</div>
-                <Col className="gap-2">
+                <Col className="gap-2 items-stretch">
                     { lists.map((list) => {
                         return (
-                            <Row key={list.id} className='gap-5'>
-                                <Link href={`/lista/${list.id}`}>
-                                    &gt; {list.name}
-                                </Link>
-                                <div className='cursor-pointer' onClick={() => removeListAndRefetch(list.id)}>[x]</div>
-                            </Row>
+                            <ListInfo
+                                key={list.id}
+                                list={list}
+                                onRemove={() => removeListAndRefetch(list.id)}
+                            />
                         )
                     }) }
                 </Col>
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    experimental: {
+        turbo: {
+            rules: {
+                '*.svg': {
+                    loaders: ['@svgr/webpack'],
+                    as: '*.js',
+                  },
+            },
+        },
+    },
 };
 
 export default nextConfig;
