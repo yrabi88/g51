5a7eb4da5876a38e34d74fd64f7f2186778896a1
yrabi88 <yrabinovich@onezerobank.com>
2025-02-08 22:55:59 +0200

    remove svgr plugin

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    experimental: {
-        turbo: {
-            rules: {
-                '*.svg': {
-                    loaders: ['@svgr/webpack'],
-                    as: '*.js',
-                  },
-            },
-        },
-    },
 };
 
 export default nextConfig;
